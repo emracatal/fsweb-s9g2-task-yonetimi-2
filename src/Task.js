8daa735c2d5f56cd0a647ce280b8502d48254ddf
emracatal <134175723+emracatal@users.noreply.github.com>
2023-08-27 11:46:38 +0300

    taskların backgroud rengi yapıldı

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,29 @@
 import React from "react";
+import { differenceInDays } from "date-fns";
 
 const Task = ({ taskObj, onComplete }) => {
-
+  const diff = differenceInDays(new Date(taskObj.deadline), new Date());
+  console.log(diff);
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">
+        son teslim:{" "}
+        <span style={{ backgroundColor: diff <= 3 ? "#ffd9d4" : "lightgray" }}>
+          {taskObj.deadline}
+        </span>
+      </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="pill" key={p}>
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>
+      )}
     </div>
   );
 };
