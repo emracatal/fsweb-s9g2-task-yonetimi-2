b6eab4cb102af32fe0128cd77bc52159803f4b55
emracatal <134175723+emracatal@users.noreply.github.com>
2023-08-27 12:50:15 +0300

    tailwind devam et

--- src/Task.js
+++ src/Task.js
@@ -11,18 +11,22 @@ const Task = ({ taskObj, onComplete }) => {
   );
   console.log(diff);
   return (
-    <div className="task">
-      <h3>{taskObj.title}</h3>
+    <div className="task py-6 px-6 bg-white border rounded-md text-sm mt-4 shadow-md">
+      <h3 className="text-lg leading-6 text-[#c8781a]">{taskObj.title}</h3>
       <div className="deadline">
-        son teslim:{" "}
-        <span style={{ backgroundColor: diff <= 3 ? "#ffd9d4" : "lightgray" }}>
+        {/* son teslim:{" "}
+        <span style={{ backgroundColor: diff <= 3 ? "#ffd9d4" : "lightgray" }}> */}
+        <span className={diff <= 3 && "bg-[#ffd9d4]"}>
           {humanReadableDeadline}
         </span>
       </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>
+          <span
+            className="inline-block text-sm py-1 px-3 border border-neutral-300 mr-1 mb-1 rounded-full"
+            key={p}
+          >
             {p}
           </span>
         ))}
